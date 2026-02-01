61623d044226330c33cb69426e1d7e37e73849a8
Go with GCP <141059279+Learnwithkarthik@users.noreply.github.com>
2026-02-02 01:28:29 +0530

    Update cloud_run_app module version to v1.0.1

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,8 +1,8 @@
 module "cloud_run_app" {
-  source = "github.com/Learnwithkarthik/terraform-gcp-modules//cloud-run?ref=v1.0.0"
+  source = "github.com/Learnwithkarthik/terraform-gcp-modules//cloud-run?ref=v1.0.1"
 
-  project_id  = var.project_id
-  region      = var.region
+  project_id   = var.project_id
+  region       = var.region
   service_name = "demo-cloudrun-dev"
   image        = "us-docker.pkg.dev/cloudrun/container/hello"
 }
